c662cd92cee0116fff7bb7cab74e4abaed2a323d
Roshani Thakor <kiara@Roshanis-MacBook-Air.local>
2022-12-08 18:20:59 -0500

    main login page error solve UI

--- LifeSave/src/LifeSave/UI/login_page.java
+++ LifeSave/src/LifeSave/UI/login_page.java
@@ -4,6 +4,7 @@
  */
 package LifeSave.UI;
 
+import UI_SystemAdmin.SystemAdminOption;
 import javax.swing.JFrame;
 import java.awt.Toolkit;
 import java.awt.event.WindowEvent;
@@ -36,13 +37,13 @@ public class login_page extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        userTextField = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jBtnLogin = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jBtnSignup_page = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        userPasswordField = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -83,14 +84,11 @@ public class login_page extends javax.swing.JFrame {
             .addGap(0, 0, Short.MAX_VALUE)
         );
 
-        jTextField1.setText("Username");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        userTextField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                userTextFieldActionPerformed(evt);
             }
         });
-
-        jTextField2.setText("Password");
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jLabel1.setText("Login");
@@ -138,11 +136,11 @@ public class login_page extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addComponent(jBtnSignup_page))
                     .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(userPasswordField, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(userTextField, javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel3Layout.createSequentialGroup()
                             .addComponent(jBtnLogin)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
                             .addComponent(jButton2))))
                 .addContainerGap(475, Short.MAX_VALUE))
         );
@@ -152,10 +150,10 @@ public class login_page extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 263, Short.MAX_VALUE)
+                .addComponent(userTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(userPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 274, Short.MAX_VALUE)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jBtnLogin)
                     .addComponent(jButton2))
@@ -195,9 +193,9 @@ public class login_page extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void userTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userTextFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_userTextFieldActionPerformed
 
     private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
        System.exit(0);
@@ -209,23 +207,14 @@ public class login_page extends javax.swing.JFrame {
     }//GEN-LAST:event_jBtnSignup_pageActionPerformed
 
     private void jBtnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnLoginActionPerformed
-       string username = usernameTextField.getText();
-       string password = passwordTextField.getText();
-        
-        if(password.contains("pass12345") && username.contains("sys_admin"))
-        {
-            usernameTextField.setText("");
-            passwordTextField.setText("");    
-             close();
-             Table t new Table();
-             t.setVisible(true);
-        }
-        else
-        { JOptionPane.showMessageDialog(null,"username or password is wrong","wrong pass",JOptionPane.ERROR_MESSAGE);
-                  usernameTextField.setText("");
-                  passwordTextField.setText(""); 
-        
-        }
+       String un = userTextField.getText();
+       String pw = userPasswordField.getText();
+       
+       if(un.equals("System Admin") && pw.equals("system")) {
+           SystemAdminOption obj=new SystemAdminOption();
+           obj.setVisible(true);
+           dispose();
+       }
         
         
         
@@ -276,7 +265,7 @@ public class login_page extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JPasswordField userPasswordField;
+    private javax.swing.JTextField userTextField;
     // End of variables declaration//GEN-END:variables
 }
