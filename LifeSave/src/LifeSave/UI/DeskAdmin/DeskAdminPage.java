bcbe42500971d05bf0255f8b59806a59b78780b3
ParthKalani <kalani.p@northeastern.edu>
2022-12-08 03:03:13 -0500

    HospitalAdmin and DeskAdmin UI created

--- LifeSave/src/LifeSave/UI/DeskAdmin/DeskAdminPage.java
+++ LifeSave/src/LifeSave/UI/DeskAdmin/DeskAdminPage.java
@@ -2,18 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package LifeSave.UI.Hospital;
+package LifeSave.UI.DeskAdmin;
+
+import LifeSave.UI.Hospital.*;
 
 /**
  *
  * @author parthkalani
  */
-public class HAdminPage extends javax.swing.JFrame {
+public class DeskAdminPage extends javax.swing.JFrame {
 
     /**
      * Creates new form HAdminPage
      */
-    public HAdminPage() {
+    public DeskAdminPage() {
         initComponents();
     }
 
@@ -132,20 +134,21 @@ public class HAdminPage extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeskAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeskAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeskAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DeskAdminPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new HAdminPage().setVisible(true);
+                new DeskAdminPage().setVisible(true);
             }
         });
     }
