632c4d8f2bd6f55f44ecdb525d824fae97996630
ParthKalani <kalani.p@northeastern.edu>
2022-12-08 20:35:52 -0500

    Few small Changes

--- LifeSave/src/MedicalEnterprise/Hospital.java
+++ LifeSave/src/MedicalEnterprise/Hospital.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package LifeSave.model.CharitableDonor;
+package MedicalEnterprise;
 
 /**
  *
  * @author parthkalani
  */
-public class UknownDonorDatabase {
+public class Hospital {
     
 }
