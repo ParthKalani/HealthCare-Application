8f5a8f93d87df07e7fc30108fed1db0941a73148
ParthKalani <kalani.p@northeastern.edu>
2022-12-11 01:31:43 -0500

    Created a class Organization in Organizations package

--- LifeSave/src/LifeSave/Organizations/Organization.java
+++ LifeSave/src/LifeSave/Organizations/Organization.java
@@ -4,10 +4,65 @@
  */
 package LifeSave.Organizations;
 
+import LifeSave.Roles.Role;
+import LifeSave.UserAccounts.UserAccountDirectory;
+import java.util.ArrayList;
+import LifeSave.Roles.Role;
+import LifeSave.UserAccounts.UserAccountDirectory;
+import java.util.ArrayList;
+
+
 /**
  *
- * @author jkoti
+ * @author parthkalani
  */
-public class Organization {
+public abstract class Organization {
+    
+    private String name;
+    private UserAccountDirectory userAccountDirectory;
+    private int orgId;
+    private static int counter = 000;
+
+    public Organization(String name) {
+    this.name = name;
+    userAccountDirectory = new UserAccountDirectory();
+    counter++;
+    orgId = counter;
+    }
+    
+    public enum Type
+    {
+     Admin("Admin");
+     
+     private String value;
+     
+     private Type(String value)
+     {
+         this.value = value;
+     }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    } 
+
+    public String getName() {
+        return name;
+    }
+    
+     public abstract ArrayList<Role> getSupportedRole();
+
+    public int getOrgId() {
+        return orgId;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
     
 }
