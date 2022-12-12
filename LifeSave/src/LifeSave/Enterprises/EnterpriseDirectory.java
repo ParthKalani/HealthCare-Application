6a6d64b7e588702bd1670d9f83ced73cf019d8ba
ParthKalani <kalani.p@northeastern.edu>
2022-12-11 22:39:58 -0500

    Changes in Enterprise Directory class of LifeSave.Enterprises Package

--- LifeSave/src/LifeSave/Enterprises/EnterpriseDirectory.java
+++ LifeSave/src/LifeSave/Enterprises/EnterpriseDirectory.java
@@ -3,11 +3,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package LifeSave.Enterprises;
+import java.util.ArrayList;
 
 /**
  *
- * @author jkoti
+ * @author parthkalani
  */
 public class EnterpriseDirectory {
+    private ArrayList<Enterprise> enterpriseList;
+    
+    public EnterpriseDirectory()
+    {
+        enterpriseList = new ArrayList<Enterprise>();
+    }
+    
+    
+    public Enterprise addEnterprise(String name, Enterprise.EnterpriseType type)
+    {
+      Enterprise enterprise = null;
+
+      if(type ==Enterprise.EnterpriseType.OrganSeeker){
+       enterprise = new OrganSeekerEnterprise(name);
+       enterpriseList.add(enterprise);     
+      }
+      else if (type == Enterprise.EnterpriseType.Government){
+            enterprise = new GovernmentEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+       else if (type == Enterprise.EnterpriseType.Charity){
+            enterprise = new CharitableTrust(name);
+            enterpriseList.add(enterprise);
+        }
+        else  if(type ==Enterprise.EnterpriseType.ServiceP){
+         enterprise = new ServiceProvider(name);
+         enterpriseList.add(enterprise);     
+        }
+      else  if(type ==Enterprise.EnterpriseType.Medical){
+         enterprise = new MedicalEnterprise(name);
+         enterpriseList.add(enterprise);
+        }
+
+      return enterprise;
+    }
+
+    public ArrayList<Enterprise> getEnterpriseList() {
+        return enterpriseList;
+    }
+    
+    public void deleteEnterprise(Enterprise enterprise)
+    {
+        enterpriseList.remove(enterprise);
+    }
     
 }
